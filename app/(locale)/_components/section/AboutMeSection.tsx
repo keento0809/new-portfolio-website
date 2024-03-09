9d8e7cd4ed92d1b693507d5d89934373e0d3ae53
KEENTO <handball0321@gmail.com>
2024-03-10 08:01:29 +0900

    Add: ProjectCard

--- app/(locale)/_components/section/AboutMeSection.tsx
+++ app/(locale)/_components/section/AboutMeSection.tsx
@@ -1,9 +1,13 @@
+import { ProjectCard } from "@/components/common/ProjectCard";
 import { Title } from "@/components/common/Title";
 
 export const AboutMeSection = () => {
   return (
     <div id="aboutMe" className="min-h-screen">
       <Title titleText="About Me" />
+      <div className="">
+        <ProjectCard />
+      </div>
     </div>
   );
 };
