d11222a059cddc6fabbcd9d64240fcb63bf56309
KEENTO <handball0321@gmail.com>
2024-03-11 08:30:17 +0900

    Add: icons for ContactMe section

--- app/(locale)/_components/section/ContactMeSection.tsx
+++ app/(locale)/_components/section/ContactMeSection.tsx
@@ -1,23 +1,66 @@
 import { Button } from "@/components/common/Button";
 import { Title } from "@/components/common/Title";
+import { EmailIcon } from "@/components/icons/EmailIcon";
+import { GitHubIcon } from "@/components/icons/GitHubIcon";
+import { LinkedInIcon } from "@/components/icons/LinkedInIcon";
+import { PhoneIcon } from "@/components/icons/PhoneIcon";
+import { BackgroundBeams } from "@/components/ui/background-beams";
 import {
+  CONTACT_METHOD_ARRAY,
   CONTACT_ME_FIRST_DESCRIPTION,
   CONTACT_ME_SECOND_DESCRIPTION,
 } from "@/constants/components/section/contactMeSection";
+import Link from "next/link";
+
+const ContactMethodIcon = ({ name }: { name: string }) => {
+  switch (name) {
+    case "PHONE": {
+      return <PhoneIcon />;
+    }
+    case "EMAIL": {
+      return <EmailIcon />;
+    }
+    case "LINKEDIN": {
+      return <LinkedInIcon />;
+    }
+    case "GITHUB": {
+      return <GitHubIcon />;
+    }
+    case "DEV": {
+      return null;
+    }
+    default: {
+      return null;
+    }
+  }
+};
 
 export const ContactMeSection = () => {
   return (
-    <div id="contact" className="min-h-screen bg-[#0A0A0A]">
-      <div className="pt-10">
-        <Title titleText="Contact Me" />
+    <div
+      id="contact"
+      className="min-h-screen w-full rounded-md bg-neutral-950 relative flex flex-col items-center justify-center antialiased"
+    >
+      <Title titleText="Contact" />
+      <div className="flex flex-col gap-12">
+        <div className="text-center text-base md:text-xl font-medium">
+          <p>{CONTACT_ME_FIRST_DESCRIPTION}</p>
+          <p>{CONTACT_ME_SECOND_DESCRIPTION}</p>
+        </div>
+        <div className="flex justify-center items-center md:gap-8">
+          {CONTACT_METHOD_ARRAY.map((method) => {
+            return (
+              <Link key={method.name} href={method.href ?? ""} target="_blank">
+                <ContactMethodIcon name={method.name} />
+              </Link>
+            );
+          })}
+        </div>
+        <div className="w-full flex justify-center items-center">
+          <Button buttonText="Say Hello" />
+        </div>
       </div>
-      <div className="text-center">
-        <p>{CONTACT_ME_FIRST_DESCRIPTION}</p>
-        <p>{CONTACT_ME_SECOND_DESCRIPTION}</p>
-      </div>
-      <div className="w-full flex justify-center items-center">
-        <Button buttonText="Say Hello" />
-      </div>
+      <BackgroundBeams />
     </div>
   );
 };
--- constants/components/section/contactMeSection.ts
+++ constants/components/section/contactMeSection.ts
@@ -1,7 +1,39 @@
+type ContactMethod = {
+  href: string | undefined;
+  name: string;
+};
+
 const CONTACT_ME_FIRST_DESCRIPTION =
   "I'm eager to find opportunities to work as a web developer." as const;
 
 const CONTACT_ME_SECOND_DESCRIPTION =
   "If you know any job opportunities, please free to contact me!" as const;
 
-export { CONTACT_ME_FIRST_DESCRIPTION, CONTACT_ME_SECOND_DESCRIPTION };
+const CONTACT_METHOD_ARRAY: ContactMethod[] = [
+  {
+    href: process.env.NEXT_PUBLIC_MY_PHONE,
+    name: "PHONE",
+  },
+  {
+    href: process.env.NEXT_PUBLIC_MY_EMAIL,
+    name: "EMAIL",
+  },
+  {
+    href: process.env.NEXT_PUBLIC_MY_LINKEDIN,
+    name: "LINKEDIN",
+  },
+  {
+    href: process.env.NEXT_PUBLIC_MY_GITHUB,
+    name: "GITHUB",
+  },
+  {
+    href: process.env.NEXT_PUBLIC_MY_DEV,
+    name: "DEV",
+  },
+];
+
+export {
+  CONTACT_ME_FIRST_DESCRIPTION,
+  CONTACT_ME_SECOND_DESCRIPTION,
+  CONTACT_METHOD_ARRAY,
+};
