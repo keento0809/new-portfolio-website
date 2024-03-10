de193802e127a9b1b3af265259cbdac48c28c9b6
KEENTO <handball0321@gmail.com>
2024-03-11 08:52:25 +0900

    Fix: styles of ContactMe section

--- app/(locale)/_components/section/ContactMeSection.tsx
+++ app/(locale)/_components/section/ContactMeSection.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { Button } from "@/components/common/Button";
 import { Title } from "@/components/common/Title";
 import { EmailIcon } from "@/components/icons/EmailIcon";
@@ -5,6 +7,7 @@ import { GitHubIcon } from "@/components/icons/GitHubIcon";
 import { LinkedInIcon } from "@/components/icons/LinkedInIcon";
 import { PhoneIcon } from "@/components/icons/PhoneIcon";
 import { BackgroundBeams } from "@/components/ui/background-beams";
+import { TextGenerateEffect } from "@/components/ui/text-generate-effect";
 import {
   CONTACT_METHOD_ARRAY,
   CONTACT_ME_FIRST_DESCRIPTION,
@@ -42,15 +45,23 @@ export const ContactMeSection = () => {
       className="min-h-screen w-full rounded-md bg-neutral-950 relative flex flex-col items-center justify-center antialiased"
     >
       <Title titleText="Contact" />
-      <div className="flex flex-col gap-12">
+      <div className="flex flex-col md:gap-8 lg:gap-14">
         <div className="text-center text-base md:text-xl font-medium">
-          <p>{CONTACT_ME_FIRST_DESCRIPTION}</p>
-          <p>{CONTACT_ME_SECOND_DESCRIPTION}</p>
+          <TextGenerateEffect
+            className="z-50"
+            words={CONTACT_ME_FIRST_DESCRIPTION}
+          />
+          <TextGenerateEffect delay={2} words={CONTACT_ME_SECOND_DESCRIPTION} />
         </div>
         <div className="flex justify-center items-center md:gap-8">
           {CONTACT_METHOD_ARRAY.map((method) => {
             return (
-              <Link key={method.name} href={method.href ?? ""} target="_blank">
+              <Link
+                key={method.name}
+                href={method.href ?? ""}
+                target="_blank"
+                className="cursor-pointer"
+              >
                 <ContactMethodIcon name={method.name} />
               </Link>
             );
