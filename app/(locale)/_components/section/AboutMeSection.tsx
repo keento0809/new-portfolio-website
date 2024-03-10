d1cf3dbc1d50083449cd0aa567d2bf232023e734
KEENTO <handball0321@gmail.com>
2024-03-10 10:28:07 +0900

    Delete: unused codes

--- app/(locale)/_components/section/AboutMeSection.tsx
+++ app/(locale)/_components/section/AboutMeSection.tsx
@@ -1,19 +1,7 @@
-import { Title } from "@/components/common/Title";
-import { useContentful } from "../../_hooks/useContentful";
-import { useEffect } from "react";
-import { MovingButton } from "@/components/common/MovingButton";
-import { TypewriteText } from "@/components/common/TypewriteText";
-
 export const AboutMeSection = () => {
-  const { getResume } = useContentful();
-  useEffect(() => {
-    getResume();
-  }, [getResume]);
   return (
     <div id="aboutMe" className="min-h-screen">
-      <Title titleText="About Me" />
-      <TypewriteText />
-      <MovingButton />
+      AboutMeSection
     </div>
   );
 };
