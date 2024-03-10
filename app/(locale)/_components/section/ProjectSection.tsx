3adfaf61679ab0a0b037a7a72d92f6f25fce11d3
KEENTO <handball0321@gmail.com>
2024-03-10 10:31:29 +0900

    Fix: cleaned up code

--- app/(locale)/_components/section/ProjectSection.tsx
+++ app/(locale)/_components/section/ProjectSection.tsx
@@ -3,7 +3,7 @@ import { Title } from "@/components/common/Title";
 export const ProjectSection = () => {
   return (
     <div id="projects" className="min-h-screen">
-      <Title titleText="Projects" />
+      ProjectSection
     </div>
   );
 };
