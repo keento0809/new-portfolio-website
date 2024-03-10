773458cd752fdedccb35b96c8ab9a33d5f9d819d
KEENTO <handball0321@gmail.com>
2024-03-10 10:02:09 +0900

    Add: constant values for TypewriteText

--- app/(locale)/_components/section/HeroSection.tsx
+++ app/(locale)/_components/section/HeroSection.tsx
@@ -1,6 +1,11 @@
 "use client";
 import { TypewriterText } from "@/components/common/TypewriteText";
 import { BackgroundBeams } from "@/components/ui/background-beams";
+import {
+  TYPEWRITER_TEXT_FIRST_PARAGRAPH,
+  TYPEWRITER_TEXT_SECOND_PARAGRAPH,
+  TYPEWRITER_TEXT_THIRD_PARAGRAPH,
+} from "@/constants/components/section/heroSection";
 
 export const HeroSection = () => {
   return (
@@ -9,36 +14,9 @@ export const HeroSection = () => {
       className="min-h-screen w-full rounded-md bg-neutral-950 relative flex flex-col items-center justify-center antialiased"
     >
       <div className="mx-auto lg:min-w-[600px] p-4">
-        <TypewriterText
-          words={[
-            {
-              text: "Hi, ",
-            },
-            {
-              text: "I'm",
-            },
-            {
-              text: "KENTO",
-              className: "text-blue-500",
-            },
-          ]}
-        />
-        <TypewriterText
-          words={[
-            {
-              text: "Web Developer",
-            },
-          ]}
-          delay={3}
-        />
-        <TypewriterText
-          words={[
-            {
-              text: "Based in Vancouver",
-            },
-          ]}
-          delay={5}
-        />
+        <TypewriterText words={TYPEWRITER_TEXT_FIRST_PARAGRAPH} />
+        <TypewriterText words={TYPEWRITER_TEXT_SECOND_PARAGRAPH} delay={3} />
+        <TypewriterText words={TYPEWRITER_TEXT_THIRD_PARAGRAPH} delay={5} />
       </div>
       <BackgroundBeams />
     </div>
---
+++ constants/components/section/heroSection.ts
@@ -0,0 +1,32 @@
+import { TypewriteWords } from "@/components/common/TypewriteText";
+
+const TYPEWRITER_TEXT_FIRST_PARAGRAPH: TypewriteWords[] = [
+  {
+    text: "Hi, ",
+  },
+  {
+    text: "I'm",
+  },
+  {
+    text: "KENTO",
+    className: "text-blue-500",
+  },
+];
+
+const TYPEWRITER_TEXT_SECOND_PARAGRAPH: TypewriteWords[] = [
+  {
+    text: "Web Developer",
+  },
+];
+
+const TYPEWRITER_TEXT_THIRD_PARAGRAPH: TypewriteWords[] = [
+  {
+    text: "Based in Vancouver",
+  },
+];
+
+export {
+  TYPEWRITER_TEXT_FIRST_PARAGRAPH,
+  TYPEWRITER_TEXT_SECOND_PARAGRAPH,
+  TYPEWRITER_TEXT_THIRD_PARAGRAPH,
+};
