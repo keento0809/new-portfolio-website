48fc1e9b91762099bd8739e4811e4bbe8313f595
KEENTO <handball0321@gmail.com>
2024-03-11 10:24:40 +0900

    Fix: bg-color of ScrollHeader

--- components/layout/ScrollHeader.tsx
+++ components/layout/ScrollHeader.tsx
@@ -5,7 +5,7 @@ import { HEADER_MENU_ITEMS } from "@/constants/components/layout/header";
 
 export function ScrollHeader() {
   return (
-    <div className="relative  w-full">
+    <div className="relative w-full">
       <FloatingNav navItems={HEADER_MENU_ITEMS} />
     </div>
   );
