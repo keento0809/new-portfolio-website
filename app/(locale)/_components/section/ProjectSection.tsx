c31cfbf3de93b69dcd61513a5f6510eab825b1f7
KEENTO <handball0321@gmail.com>
2024-03-14 11:52:31 +0900

    Add: basics of ProjectLayout component

--- app/(locale)/_components/section/ProjectSection.tsx
+++ app/(locale)/_components/section/ProjectSection.tsx
@@ -1,7 +1,20 @@
+"use client";
+
+import { Title } from "@/components/common/Title";
+import { BackgroundBeams } from "@/components/ui/background-beams";
+import { ProjectLayout } from "./project/ProjectLayout";
+
 export const ProjectSection = () => {
   return (
-    <div id="projects" className="min-h-screen bg-neutral-950">
-      ProjectSection
+    <div
+      id="projects"
+      className="min-h-screen w-full rounded-md bg-neutral-950 relative flex flex-col items-center justify-center antialiased"
+    >
+      <Title titleText="Projects" />
+      <div className="z-30 py-10">
+        <ProjectLayout />
+      </div>
+      <BackgroundBeams />
     </div>
   );
 };
