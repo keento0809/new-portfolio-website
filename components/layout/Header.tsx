bcc836d758a32d8dc75f914fab3d688c02f2c06e
KEENTO <handball0321@gmail.com>
2024-03-21 06:31:16 +0900

    Add: links to Header and ContactMe section

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -1,32 +1,30 @@
 "use client";
 
-import { useState } from "react";
 import { Menu } from "../ui/navbar-menu";
 import { cn } from "@/utils/cn";
 import Link from "next/link";
 import { HEADER_MENU_ITEMS } from "@/constants/components/layout/header";
 import { usePathname } from "next/navigation";
+import { useHeader } from "./_hooks/useHeader";
 
-export function Header() {
-  return <Navbar />;
-}
-
-function Navbar({ className }: { className?: string }) {
-  const [active, setActive] = useState<string | null>(null);
+export const Header = ({ className }: { className?: string }) => {
   const pathname = usePathname();
+  const { resumeURL } = useHeader();
   return (
     <div className={cn("absolute top-2 inset-x-0 mx-auto z-50", className)}>
       <Menu>
+        {/* TODO: fix this later */}
         <Link href={"/"}>K.H</Link>
         <div className="text-primary-color text-xs w-full flex justify-end items-center gap-x-12 ">
           {HEADER_MENU_ITEMS.map((menu) => (
             <Link
               key={menu.name}
-              href={menu.link}
+              href={menu.name !== "Resume" ? menu.link : resumeURL}
               className={cn(
                 "block hover:text-blue-500 ease-in-out transition-all",
                 menu.link === pathname && "text-blue-500"
               )}
+              target={menu.target}
             >
               {menu.name}
             </Link>
@@ -35,4 +33,4 @@ function Navbar({ className }: { className?: string }) {
       </Menu>
     </div>
   );
-}
+};
