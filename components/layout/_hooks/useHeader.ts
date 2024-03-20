3ef333180de37b44c61813fa4410d77d85b9c854
KEENTO <handball0321@gmail.com>
2024-03-21 07:49:29 +0900

    Fix: hook test again

--- components/layout/_hooks/useHeader.ts
+++ components/layout/_hooks/useHeader.ts
@@ -3,16 +3,17 @@ import { useContentful } from "@/app/(locale)/_hooks/useContentful";
 import { useEffect, useState } from "react";
 
 export const useHeader = () => {
-  const { getResume } = useContentful();
+  //   const { getResume } = useContentful();
   const [resumeURL, setResumeURL] = useState<string>("");
 
   useEffect(() => {
-    getResume()
-      .then((res: IResumeFields | undefined) => {
-        setResumeURL(res ? res.myResume.fields.file.url : "");
-      })
-      .catch((error) => console.log(error));
-  }, [getResume]);
+    // getResume()
+    //   .then((res: IResumeFields | undefined) => {
+    //     setResumeURL(res ? res.myResume.fields.file.url : "");
+    //   })
+    //   .catch((error) => console.log(error));
+    setResumeURL("aaa");
+  }, []);
 
   return { resumeURL };
 };
