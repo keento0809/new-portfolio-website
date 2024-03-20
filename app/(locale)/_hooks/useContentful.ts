f75aa9ef14f764141c7721d5b7925e2f2e3254d8
KEENTO <handball0321@gmail.com>
2024-03-21 08:23:56 +0900

    Fix: test fixing deploy error

--- app/(locale)/_hooks/useContentful.ts
+++ app/(locale)/_hooks/useContentful.ts
@@ -6,7 +6,7 @@ import {
 import { createClient } from "contentful";
 
 export const client = createClient({
-  space: process.env.NEXT_PUBLIC_CONTENTFUL_SPACE_ID ?? "",
+  space: process.env.NEXT_PUBLIC_CONTENTFUL_SPACE ?? "",
   accessToken: process.env.NEXT_PUBLIC_CONTENTFUL_ACCESS_TOKEN ?? "",
   host: process.env.NEXT_PUBLIC_CONTENTFUL_HOST,
 });
