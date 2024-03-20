02356b321156a456fdd2127569340fe86af3c762
KEENTO <handball0321@gmail.com>
2024-03-21 06:09:54 +0900

    Add: ResumeIcon

---
+++ components/icons/index.ts
@@ -0,0 +1,5 @@
+export { EmailIcon } from "./EmailIcon";
+export { ResumeIcon } from "./ResumeIcon";
+export { GitHubIcon } from "./GitHubIcon";
+export { LinkedInIcon } from "./LinkedInIcon";
+export { PhoneIcon } from "./PhoneIcon";
