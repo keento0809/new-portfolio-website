1d72c59d74f4e204ff73354672265299368925c0
KEENTO <handball0321@gmail.com>
2024-03-21 07:45:21 +0900

    Fix: hook again and again and again

--- components/layout/_hooks/useHeader.ts
+++ components/layout/_hooks/useHeader.ts
@@ -1,18 +1,19 @@
-import { IResumeFields } from "@/@types/generated/contentful";
-import { useContentful } from "@/app/(locale)/_hooks/useContentful";
-import { useEffect, useState } from "react";
+// import { IResumeFields } from "@/@types/generated/contentful";
+// import { useContentful } from "@/app/(locale)/_hooks/useContentful";
+// import { useEffect, useState } from "react";
 
 export const useHeader = () => {
-  const { getResume } = useContentful();
-  const [resumeURL, setResumeURL] = useState<string>("");
+  //   const { getResume } = useContentful();
+  //   const [resumeURL, setResumeURL] = useState<string>("");
 
-  useEffect(() => {
-    getResume()
-      .then((res: IResumeFields | undefined) => {
-        setResumeURL(res ? res.myResume.fields.file.url : "");
-      })
-      .catch((error) => console.log(error));
-  }, [getResume]);
+  //   useEffect(() => {
+  //     getResume()
+  //       .then((res: IResumeFields | undefined) => {
+  //         setResumeURL(res ? res.myResume.fields.file.url : "");
+  //       })
+  //       .catch((error) => console.log(error));
+  //   }, [getResume]);
+  const resumeURL = "aaa";
 
   return { resumeURL };
 };
