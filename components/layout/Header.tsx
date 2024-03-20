29a729315062b7bf11b26a972f26d58a02e18696
KEENTO <handball0321@gmail.com>
2024-03-21 07:30:43 +0900

    Fix: stop importing hook

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -6,7 +6,6 @@ import { cn } from "@/utils/cn";
 import Link from "next/link";
 import { HEADER_MENU_ITEMS } from "@/constants/components/layout/header";
 import { usePathname } from "next/navigation";
-import { useContentful } from "@/app/(locale)/_hooks/useContentful";
 import { useHeader } from "./_hooks/useHeader";
 
 export function Header() {
@@ -16,7 +15,7 @@ export function Header() {
 function Navbar({ className }: { className?: string }) {
   const [active, setActive] = useState<string | null>(null);
   const pathname = usePathname();
-  const { resumeURL } = useHeader();
+  // const { resumeURL } = useHeader();
   return (
     <div className={cn("absolute top-2 inset-x-0 mx-auto z-50", className)}>
       <Menu>
