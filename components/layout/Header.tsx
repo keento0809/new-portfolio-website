acb52d9719badca6ab47cfe63340a17440c1dbf4
KEENTO <handball0321@gmail.com>
2024-03-21 07:12:49 +0900

    Fix: atributes of Header

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -29,9 +29,9 @@ function Navbar({ className }: { className?: string }) {
                 "block hover:text-blue-500 ease-in-out transition-all",
                 menu.link === pathname && "text-blue-500"
               )}
-              target={menu.target}
-              passHref
-              rel="noopener noreferrer"
+              // target={menu.target}
+              // passHref
+              // rel="noopener noreferrer"
             >
               {menu.name}
             </Link>
