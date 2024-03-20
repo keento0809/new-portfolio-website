f4d128028583b7c0a3c78c5a0381cd5a239c00a6
KEENTO <handball0321@gmail.com>
2024-03-21 07:47:29 +0900

    Fix: hook test

--- components/layout/_hooks/useHeader.ts
+++ components/layout/_hooks/useHeader.ts
@@ -1,19 +1,18 @@
-// import { IResumeFields } from "@/@types/generated/contentful";
-// import { useContentful } from "@/app/(locale)/_hooks/useContentful";
-// import { useEffect, useState } from "react";
+import { IResumeFields } from "@/@types/generated/contentful";
+import { useContentful } from "@/app/(locale)/_hooks/useContentful";
+import { useEffect, useState } from "react";
 
 export const useHeader = () => {
-  //   const { getResume } = useContentful();
-  //   const [resumeURL, setResumeURL] = useState<string>("");
+  const { getResume } = useContentful();
+  const [resumeURL, setResumeURL] = useState<string>("");
 
-  //   useEffect(() => {
-  //     getResume()
-  //       .then((res: IResumeFields | undefined) => {
-  //         setResumeURL(res ? res.myResume.fields.file.url : "");
-  //       })
-  //       .catch((error) => console.log(error));
-  //   }, [getResume]);
-  const resumeURL = "aaa";
+  useEffect(() => {
+    getResume()
+      .then((res: IResumeFields | undefined) => {
+        setResumeURL(res ? res.myResume.fields.file.url : "");
+      })
+      .catch((error) => console.log(error));
+  }, [getResume]);
 
   return { resumeURL };
 };
