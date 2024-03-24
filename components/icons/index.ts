17575498da5bdda1cb9594b23409f51fbad3fa4c
KEENTO <handball0321@gmail.com>
2024-03-25 01:57:03 +0900

    Fix: base of HeroSection for mobile version

--- components/icons/index.ts
+++ components/icons/index.ts
@@ -3,3 +3,4 @@ export { ResumeIcon } from "./ResumeIcon";
 export { GitHubIcon } from "./GitHubIcon";
 export { LinkedInIcon } from "./LinkedInIcon";
 export { PhoneIcon } from "./PhoneIcon";
+export { MenuIcon } from "./MenuIcon";
