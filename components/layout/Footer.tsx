ccb4a9f5b2067b2e36f3105202662154dc82fc14
KEENTO <handball0321@gmail.com>
2024-03-25 02:19:45 +0900

    Fix: style

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -4,7 +4,7 @@ import { FOOTER_TEXT } from "@/constants/components/layout/footer";
 
 export const Footer = () => {
   return (
-    <div className="pb-3 bg-[#0a0a0a] w-full text-xs rounded-md flex flex-col items-center justify-center antialiased">
+    <div className="pb-3 bg-[#181818] w-full text-xs rounded-md flex flex-col items-center justify-center antialiased">
       <p className="text-primary-color">{FOOTER_TEXT}</p>
     </div>
   );
