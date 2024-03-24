dd566125cd6a116804f7d556affdab5afbb704e0
KEENTO <handball0321@gmail.com>
2024-03-25 04:53:29 +0900

    Add: link

--- app/(locale)/_components/HeroSection.tsx
+++ app/(locale)/_components/HeroSection.tsx
@@ -30,7 +30,7 @@ export const HeroSection = () => {
               Web Developer based in Vancouver
             </div>
             <button className="border-neutral-200 dark:border-neutral-500 rounded-full w-fit text-primary-color font-medium px-4 py-2 lg:px-6 lg:py-3">
-              <Link href='/projects'>Explore</Link>
+              <Link href="/projects">Explore</Link>
             </button>
           </div>
         </motion.div>
