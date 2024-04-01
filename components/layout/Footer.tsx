9d1ee313833f77af52b7f3b1317759ef7d254198
KEENTO <handball0321@gmail.com>
2024-04-02 08:12:08 +0900

    Fix: whitespaces of Footer

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -7,7 +7,7 @@ export const Footer = () => {
   return (
     <div
       className={cn(
-        "py-6 min-h-[unset] lg:py-10 bg-white/90 text-neutral-950 w-full text-xs flex flex-col items-center justify-center"
+        "py-16 min-h-[unset] lg:py-12 text-primary-color w-full text-xs flex flex-col items-center justify-center font-semibold"
       )}
     >
       <p>{FOOTER_TEXT}</p>
