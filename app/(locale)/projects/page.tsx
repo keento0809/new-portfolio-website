2942879fc19f6c3792bd3155b80f32f8f20f1ada
KEENTO <handball0321@gmail.com>
2024-04-04 07:44:14 +0900

    Add: metadata for pages

--- app/(locale)/projects/page.tsx
+++ app/(locale)/projects/page.tsx
@@ -1,4 +1,9 @@
+import { Metadata } from "next";
 import { ProjectSection } from "./_components/ProjectSection";
+
+export const metadata: Metadata = {
+  title: "Projects",
+};
 
 export default function Projects() {
   return <ProjectSection />;
