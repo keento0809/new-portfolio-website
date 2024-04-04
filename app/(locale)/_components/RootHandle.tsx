4cdd432acbe90ee04b2528f1c0795fb71ebcbb9f
KEENTO <handball0321@gmail.com>
2024-04-04 10:56:32 +0900

    Add: favicon

--- app/(locale)/_components/RootHandle.tsx
+++ app/(locale)/_components/RootHandle.tsx
@@ -1,5 +1,6 @@
 import { Footer } from "@/components/layout/Footer";
 import { Header } from "@/components/layout/Header";
+import Head from "next/head";
 import { FC } from "react";
 
 type RootHandleProps = {
@@ -9,6 +10,9 @@ type RootHandleProps = {
 export const RootHandle: FC<RootHandleProps> = ({ children }) => {
   return (
     <>
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <Header />
       <div className="">{children}</div>
       <Footer />
