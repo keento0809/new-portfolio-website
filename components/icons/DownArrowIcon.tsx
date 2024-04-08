47daac27a5c65b80cb46fbb37456f1f72424fe91
KEENTO <handball0321@gmail.com>
2024-04-08 11:34:58 +0900

    Fix: animation of DownArrowIcon for mobile

--- components/icons/DownArrowIcon.tsx
+++ components/icons/DownArrowIcon.tsx
@@ -7,13 +7,13 @@ export const DownArrowIcon = ({ className }: { className?: string }) => {
       xmlns="http://www.w3.org/2000/svg"
       fill="none"
       viewBox="0 0 24 24"
-      stroke-width="1.5"
+      strokeWidth="1.5"
       stroke="currentColor"
       className={cn("w-6 h-6", className)}
     >
       <path
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeLinecap="round"
+        strokeLinejoin="round"
         d="M19.5 13.5 12 21m0 0-7.5-7.5M12 21V3"
       />
     </svg>
