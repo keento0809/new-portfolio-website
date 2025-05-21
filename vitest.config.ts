a88d54b09d49ee632e5fe126dc898c20ef88b711
KEENTO <handball0321@gmail.com>
2025-05-21 15:49:49 +0900

    Fix: delete unused comment

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,7 +2,6 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vitest/config";
 import { resolve } from "node:path";
 
-// More info at: https://storybook.js.org/docs/writing-tests/test-addon
 export default defineConfig({
   plugins: [react()],
   test: {
@@ -11,7 +10,7 @@ export default defineConfig({
   },
   resolve: {
     alias: {
-      "@": resolve(__dirname, "."), // project root
+      "@": resolve(__dirname, "."),
     },
   },
 });
